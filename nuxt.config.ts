a40582b8831e00e902ede8bf4917ef8255dfc6d2
alexplusplus <alexanderekb@gmail.com>
2024-08-07 18:52:38 +0400

    Add FBA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({modules: [
-  '@nuxtjs/tailwindcss','@nuxt/icon'
-],
+export default defineNuxtConfig({modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', "nuxt-gtag"],
   compatibilityDate: '2024-04-03',
   app: {
     head: {
